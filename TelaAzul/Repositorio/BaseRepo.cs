a403b977da925b39c5f02a6aa4de81a99abc30df
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-02-16 19:30:57 -0300

    upload

---
+++ TelaAzul/Repositorio/BaseRepo.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repositorio
+{
+    public class BaseRepo
+    {
+
+    }
+}
---
+++ TelaAzul/TelaAzul/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TelaAzul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
