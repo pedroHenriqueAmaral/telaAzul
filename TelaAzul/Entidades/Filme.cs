d692109ead106fa9f007ec0e812277dbc5733461
Pedro Amaral <105731672+pedroHenriqueAmaral@users.noreply.github.com>
2023-02-28 14:27:25 -0300

    upload

---
+++ TelaAzul/Entidades/Filme.cs
@@ -0,0 +1,17 @@
+﻿namespace Entidades
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+        /* ... */
+
+
+
+        /* Relacionamento | Lado de "1" */
+        public int IdCategoria { get; set; }
+        public virtual Categoria Categoria { get; set; }
+        
+    }
+}
