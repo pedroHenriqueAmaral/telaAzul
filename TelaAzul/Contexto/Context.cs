17ceb6280776b56da0998c6b1d6e34f014b8b431
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-03-01 20:56:42 -0300

    update context.cs
    connect database

--- TelaAzul/Contexto/Context.cs
+++ TelaAzul/Contexto/Context.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Entidades;
+﻿using Entidades;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contexto
 {
@@ -8,6 +8,45 @@ namespace Contexto
         public Context() 
         {
             this.Database.EnsureCreated();
+        }
+
+        /* DbSets */
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+
+        /* Conexão com SQL Server */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var Conn = @"Server=NomeServer;
+                         DataBase=NomeBanco;
+                         integrated security=true";
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Conn);
+            }
+        }
+
+        /* Validações dos campos do Banco  */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entidade =>
+            {
+                entidade.HasKey(e => e.Id);
+                entidade.Property(e => e.Nome).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Filme>(entidade =>
+            {
+                entidade.HasKey(e => e.Id);
+                entidade.Property(e => e.Descricao).HasMaxLength(150);
+                
+                /* Relacionamento Filme-Categoria  */
+                entidade.HasOne(e => e.Categoria).WithMany(c => c.Filme)
+                .HasConstraintName("FK_Filme_Categoria").OnDelete(DeleteBehavior.NoAction);
+            });
         }
     }
 }
