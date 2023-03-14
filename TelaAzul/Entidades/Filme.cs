e3f782d2aa3805f8ee254e34f556408ef1050dc8
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-03-14 15:54:52 -0300

    Atualizado a Entidade de Filmes
    Acrescentado novas propriedades get/set, feito a mudança de Categoria para Gênero.

--- TelaAzul/Entidades/Filme.cs
+++ TelaAzul/Entidades/Filme.cs
@@ -3,15 +3,21 @@
     public class Filme
     {
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Titulo { get; set; }
+        public string Titulo_original { get; set; }
+        public string Sinopse { get; set; }
+        public string Duracao { get; set; } // em minutos
 
-        /* ... */
-
+        // ?????????????????????????????????????????????
+        public string Tipo { get; set; } // Normal ou 3d
+        public string Audio { get; set; } // Dublado ou Legendado
 
 
         /* Relacionamento | Lado de "1" */
-        public int IdCategoria { get; set; }
-        public virtual Categoria Categoria { get; set; }
-        
+        public int IdGenero { get; set; }
+        public virtual Genero Generos { get; set; }
+
+        /* Elenco, Direção, Ingresso */
+        //...
     }
 }
