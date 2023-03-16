e4d71845537b22bb380b2e89f32079e4850e0c43
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-03-16 19:41:13 -0300

    Adicionado relacionamento com Entidade de Venda

--- TelaAzul/Entidades/Cliente.cs
+++ TelaAzul/Entidades/Cliente.cs
@@ -9,12 +9,13 @@ namespace Entidades
     public class Cliente
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string ? Nome { get; set; }
         public DateTime Data_Nascimento { get; set; }
-        public string Tipo_Cliente { get; set; } // Estudante, Criança, Comum
-        public string Email { get; set; } // usado para login (possibilidade para newsletter)
-        public string Senha { get; set; }
+        public string ? Tipo_Cliente { get; set; } // Estudante, Criança, Comum
+        public string ? Email { get; set; } // usado para login (possibilidade para newsletter)
+        public string ? Senha { get; set; }
         
         // ingressos => venda (muitos-muitos)
+        public virtual ICollection <Venda> ? Vendas { get; set; }
     }
 }
