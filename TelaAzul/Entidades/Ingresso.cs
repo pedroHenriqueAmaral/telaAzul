7812e2fc11a28bc1791672d1fedf73ba89ad4c11
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-03-15 21:39:51 -0300

    Acrescentado propriedades na Entidade Ingresso

--- TelaAzul/Entidades/Ingresso.cs
+++ TelaAzul/Entidades/Ingresso.cs
@@ -9,12 +9,17 @@ namespace Entidades
     public class Ingresso
     {
         public int Id { get; set; }
+        public string ? Tipo { get; set; } // Normal ou 3D  
+        public virtual ICollection<Sala> ? Sala { get; set; }
+        public DateTime Data_Hora_Sessao { get; set; }
 
-        public virtual ICollection<Sala> Sala { get; set; }
-        
-        // Cliente
-        // Data/Hora
-        // Filme
+        // valor do ingresso
+        public float Valor { get; set; }
+
+        // Vendas = Cliente + Filme + Ingresso
+        public virtual ICollection<Venda> Vendas { get; set; }
+
+
 
     }
 }
