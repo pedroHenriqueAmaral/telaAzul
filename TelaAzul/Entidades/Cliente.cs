eb4a5429a37b58209605fde2f4d1271176f9a3cf
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-03-15 21:41:27 -0300

    Criados entidades para Clientes e Funcionários

---
+++ TelaAzul/Entidades/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+        public string Tipo_Cliente { get; set; } // Estudante, Criança, Comum
+        public string Email { get; set; } // usado para login (possibilidade para newsletter)
+        public string Senha { get; set; }
+        
+        // ingressos => venda (muitos-muitos)
+    }
+}
---
+++ TelaAzul/Entidades/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Funcionario // ????
+    {
+        public int Id { get; set; }
+        public string ? Nome_Completo { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+
+        public DateTime Data_Admissao { get; set; }
+        public string Cargo { get; set; }
+        public string Salario_Fixo { get; set; }
+    }
+}
