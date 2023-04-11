7fdaa38fb6033a4c3adabdb2b2c09bc7fa41b82c
Pedro Amaral <pedro.martines.amaral@gmail.com>
2023-04-11 17:20:37 -0300

    HomeSite Index e Listar

---
+++ TelaAzul/TelaAzul/Controllers/HomeSiteController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelaAzul.Models;
+
+namespace TelaAzul.Controllers
+{
+    public class HomeSiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            FilmeModel model = new FilmeModel();
+            return View(model.Listar());
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            FilmeModel model = new FilmeModel();
+            FilmeModel produto = model.Selecionar(id);
+            return View(produto);
+        }
+    }
+}
